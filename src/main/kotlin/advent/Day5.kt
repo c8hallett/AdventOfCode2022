813f6278a45bf6d241899f63e2f49923df58e149
Caitlyn Hallett <65869260+c8hallett@users.noreply.github.com>
2022-12-05 11:31:40 -0600

    Day 5

---
+++ src/main/kotlin/advent/Day5.kt
@@ -0,0 +1,90 @@
+package advent
+
+import ResourceFetcher
+import checkValidMatch
+
+fun main(args: Array<String>) {
+    Day5.part2()
+}
+
+object Day5 {
+    private val moveRegex = Regex("move (\\d+) from (\\d+) to (\\d+)")
+    fun part1() {
+        var stackComplete = false
+        val stacks = mutableListOf<MutableList<Char>>()
+        ResourceFetcher.forEachLine("day_5_input.txt") { row ->
+            when{
+                !stackComplete -> {
+                    if (row.all { it.isDigit() || it.isWhitespace() }) {
+                        stackComplete = true
+                    } else {
+                        row.chunked(4).forEachIndexed { index, byte ->
+                            if(byte.isNotBlank()) {
+                                // for index of 4, size should be 5
+                                while(stacks.size <= index) stacks.add(mutableListOf())
+                                stacks[index].add(0, byte.first{ it.isLetter() })
+                            }
+                        }
+                    }
+                }
+                row.isNotBlank() -> {
+                    moveRegex.checkValidMatch(row){ (c, fi, ti) ->
+                        val count = c.toInt()
+                        val fromIndex = fi.toInt() - 1
+                        val toIndex = ti.toInt() - 1
+
+                        repeat(count) {
+                            stacks[toIndex].add(stacks[fromIndex].removeLast())
+                        }
+                    }
+                }
+            }
+        }
+
+        val answer = stacks.fold("") { message, stack ->
+            message + if(stack.isEmpty())" " else stack.last()
+        }
+        println(answer)
+    }
+
+    fun part2() {
+        var stackComplete = false
+        val stacks = mutableListOf<MutableList<Char>>()
+        ResourceFetcher.forEachLine("day_5_input.txt") { row ->
+            when{
+                !stackComplete -> {
+                    if (row.all { it.isDigit() || it.isWhitespace() }) {
+                        stackComplete = true
+                    } else {
+                        row.chunked(4).forEachIndexed { index, byte ->
+                            if(byte.isNotBlank()) {
+                                // for index of 4, size should be 5
+                                while(stacks.size <= index) stacks.add(mutableListOf())
+                                stacks[index].add(0, byte.first{ it.isLetter() })
+                            }
+                        }
+                    }
+                }
+                row.isNotBlank() -> {
+                    moveRegex.checkValidMatch(row){ (c, fi, ti) ->
+                        val count = c.toInt()
+                        val fromIndex = fi.toInt() - 1
+                        val toIndex = ti.toInt() - 1
+
+                        val removed = mutableListOf<Char>().apply {
+                            repeat(count){
+                                add(0, stacks[fromIndex].removeLast())
+                            }
+                        }
+                        stacks[toIndex].addAll(removed)
+                    }
+                }
+            }
+        }
+
+        val answer = stacks.fold("") { message, stack ->
+            message + if(stack.isEmpty())" " else stack.last()
+        }
+        println(answer)
+    }
+}
